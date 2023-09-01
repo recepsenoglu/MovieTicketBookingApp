4303e713767839e52fea485260cb5302c088a80a
Recep Oğuzhan Şenoğlu <recep.senoglu.34@gmail.com>
2023-09-01 17:00:03 +0300

    Initial Commit

---
+++ MovieTicketBooking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieTicketBooking
+//
+//  Created by Recep Oğuzhan Şenoğlu on 1.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
